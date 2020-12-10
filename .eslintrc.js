6fa6c34739bc3b8236e2c4949b288df1e5a5b3ce
manato.kuroda <a7108100@gmail.com>
2020-12-10 20:47:25 +0900

    chore: add recoil folder

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+  ],
+  plugins: ['babel'],
+  rules: {
+    quotes: ['error', 'single'],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'all',
+      },
+    ],
+    // '@typescript-eslint/no-angle-bracket-type-assertion': 'off',
+    // '@typescript-eslint/consistent-type-assertions': 'warn',
+  },
+}
